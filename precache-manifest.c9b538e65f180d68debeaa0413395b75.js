1a6eca9a4b936e8021ad7b4f4b1c4f189c3e1874
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2020-06-24 16:43:15 +0200

    Updates

---
+++ precache-manifest.c9b538e65f180d68debeaa0413395b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0cd3f465e959fea48c38d59d1b94c0",
+    "url": "/ordvirr/index.html"
+  },
+  {
+    "revision": "901ac3a6c844bbf56962",
+    "url": "/ordvirr/static/css/main.a6217612.chunk.css"
+  },
+  {
+    "revision": "a438315f579c8e80e704",
+    "url": "/ordvirr/static/js/2.7b3cdd61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ordvirr/static/js/2.7b3cdd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901ac3a6c844bbf56962",
+    "url": "/ordvirr/static/js/main.0bc059f1.chunk.js"
+  },
+  {
+    "revision": "c6d65a64e8b4f441d6e8",
+    "url": "/ordvirr/static/js/runtime-main.ea996836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ordvirr/static/media/logo.5d5d9eef.svg"
+  }
+]);
